32d0f431c41aff8c8fe087898adeddc9a4d37863
petrovick <petrovickg@github.com>
2015-06-20 18:20:33 -0300

    JSP Servlet Controller

--- src/main/java/com/controller/JetSkyServlet.java
+++ src/main/java/com/controller/JetSkyServlet.java
@@ -2,11 +2,12 @@ package com.controller;
 import java.io.IOException;
 import java.io.PrintWriter;
 
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-
+@WebServlet("/login")
 public class JetSkyServlet extends HttpServlet{
 
 	public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException
