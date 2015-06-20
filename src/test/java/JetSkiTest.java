58e67202f7d9fff9dbe23553d87fde1a0cc503d9
petrovick <petrovickg@github.com>
2015-06-20 20:37:28 -0300

    teste junit

--- src/test/java/JetSkiTest.java
+++ src/test/java/JetSkiTest.java
@@ -8,14 +8,22 @@ import com.controller.*;
 public class JetSkiTest
 {
 	@Test
-	public void testSum() {
+	public void testDescricao() {
 		JetSkiServlet sv = new JetSkiServlet();
 		JetSki j = new JetSki(null, "Desc", 1, 1);
-		
-		
 		String erroQueDeveAcontecer = "Sua descrição deve ter acima de 10 letras";
 		String erroRetornado = sv.validar(j.getDescricao(), j.getHp() + "", j.getPeso() + "");
 		assertEquals("São Iguais", erroQueDeveAcontecer, erroRetornado);
 	}
+	
+	@Test
+	public void testCriouNovo() {
+		JetSkiApplication sa = new JetSkiApplication();
+		JetSki j = new JetSki(null, "DesctestCriouNovo", 1, 1);
+		String erroRetornado = sa.create(j);
+		assertEquals("São Iguais", erroRetornado, "Nao funcionou");
+	}
+	
+	
 
 }
