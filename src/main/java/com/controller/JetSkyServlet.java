2dc074a244b484e261b7a3053d96423e6f7d98ad
petrovick <petrovickg@github.com>
2015-06-20 18:30:14 -0300

    teste

--- src/main/java/com/controller/JetSkyServlet.java
+++ src/main/java/com/controller/JetSkyServlet.java
@@ -7,26 +7,25 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-@WebServlet("/login")
+@WebServlet(urlPatterns ={"/login"})
 public class JetSkyServlet extends HttpServlet
 {
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -7321681160776582861L;
+
 	public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException
 	{
-		String username = request.getParameter("username");
-        String password = request.getParameter("password");
 		response.setContentType("text/html");
 		PrintWriter out = response.getWriter();
-		String c = request.getParameter("cor");
 		out.println("<h1>Cervejas</h1>");
 	}
 
 	public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException
 	{
-		String username = request.getParameter("username");
-        String password = request.getParameter("password");
 		response.setContentType("text/html");
 		PrintWriter out = response.getWriter();
-		String c = request.getParameter("cor");
 		out.println("<h1>Refrigerante</h1>");
 	}
 	
