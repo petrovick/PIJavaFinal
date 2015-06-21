ea90ec6e468d82db01ade6822a5da6f5d4cf646f
petrovick <petrovickg@github.com>
2015-06-21 00:14:16 -0300

    Test fixed.

--- src/test/java/JetSkiTest.java
+++ src/test/java/JetSkiTest.java
@@ -21,8 +21,8 @@ public class JetSkiTest
 		JetSkiApplication sa = new JetSkiApplication();
 		JetSki j = new JetSki(null, "DesctestCriouNovo", 1, 1);
 		String erroRetornado = sa.create(j);
-		assertEquals("São Iguais", erroRetornado, "Nao funcionou");
-		//assertEquals("São Iguais", erroRetornado, null);//Teste falha
+		//assertEquals("São Iguais", erroRetornado, "Nao funcionou");//Teste falha
+		assertEquals("São Iguais", erroRetornado, null);
 	}
 	
 	
